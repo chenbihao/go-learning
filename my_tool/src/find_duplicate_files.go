7e7d071a573c5dc52b9c8620594f2d39b326dee7
被水淹没 <994523036@qq.com>
2023-10-06 17:43:49 +0800

    自用小工具-查找重复文件

---
+++ my_tool/src/find_duplicate_files.go
@@ -0,0 +1,61 @@
+package main
+
+import (
+	"fmt"
+	"os"
+	"path/filepath"
+	"sort"
+)
+
+func formatKB(bytes int64) string {
+	kb := (bytes + 1023) / 1024 // 使用整数除法并四舍五入
+	return fmt.Sprintf("%d KB", kb)
+}
+
+func findDuplicateFiles(rootDir string) {
+	fileSizeMap := make(map[int64][]string)
+
+	// 遍历文件夹
+	err := filepath.Walk(rootDir, func(path string, info os.FileInfo, err error) error {
+		if err != nil {
+			return err
+		}
+		if !info.IsDir() {
+			// 记录文件大小和路径
+			size := info.Size()
+			fileSizeMap[size] = append(fileSizeMap[size], path)
+		}
+		return nil
+	})
+
+	if err != nil {
+		fmt.Println("遍历文件夹时出错:", err)
+		return
+	}
+
+	// 提取并排序文件大小
+	sizes := make([]int64, 0, len(fileSizeMap))
+	for size := range fileSizeMap {
+		sizes = append(sizes, size)
+	}
+	sort.Slice(sizes, func(i, j int) bool {
+		return sizes[i] > sizes[j]
+	})
+
+	// 输出重复的文件（按文件大小从大到小）
+	for _, size := range sizes {
+		paths := fileSizeMap[size]
+		if len(paths) > 1 {
+			fmt.Printf("-------------------------\n")
+			fmt.Printf("文件大小 %s KB 的重复文件:\n", formatKB(size))
+			for _, path := range paths {
+				fmt.Println("	" + path)
+			}
+		}
+	}
+}
+
+func main() {
+	rootDir := `Q:\其他\文件\`
+	findDuplicateFiles(rootDir)
+}
